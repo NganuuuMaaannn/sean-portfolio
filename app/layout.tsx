5a568859a270d752a7f6fd2d6e3133d799f44ddd
NganuuuMaaannn <seanmichael.doinog@hcdc.edu.ph>
2025-12-11 18:55:27 +0800

    Added LenisProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import LenisProvider from "./components/LenisProvider";
 import Script from "next/script";
 
 const geistSans = Geist({
@@ -32,7 +33,9 @@ export default function RootLayout({
       </head>
 
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {children}
+        <LenisProvider>
+          {children}
+        </LenisProvider>
       </body>
     </html>
   );
